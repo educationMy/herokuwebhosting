356a7ab567a46fa77f3816240061d910313fa1ee
educationMy <myeducationmay211995@gmail.com>
2021-08-29 15:55:12 +0530

    minor change in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  const msg = 'Hello Node! This is my first node app hosted on heroku\n'
   res.end(msg);
 });
 
