a58e5c8128a0e5aaca73bba815d21f6857b69041
educationMy <myeducationmay211995@gmail.com>
2021-09-02 06:53:35 +0530

    index.js changed

--- index.js
+++ index.js
@@ -1,12 +1,51 @@
 const http = require('http');
-const port = process.env.PORT || 3000;
+const fs = require('fs');
+const path = require('path');
+
+const hostname = "localhost";
+const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node! This is my first node app hosted on heroku\n'
-  res.end(msg);
+
+    console.log('request for ' + req.url + 'by method ' + req.method);
+
+    if (req.method == 'GET') {
+        var fileURL;
+        if (req.url == '/') {
+            fileURL = "/index.html";
+        } else { fileURL = req.url }
+
+        var filePath = path.resolve('./public' + fileURL);
+
+        const fileExt = path.extname(filePath);
+
+        if (fileExt == '.html') {
+            fs.exists(filePath, (exists) => {
+                if (!exists) {
+                    res.statusCode = 404;
+                    res.setHeader('Content-type', 'text/html');
+                    res.end('<html> <body> <h1> error 404:'+ fileURL+ ' does not exists </h1></body></html>');
+                }
+                else{
+                    res.statusCode = 200;
+                    res.setHeader('Content-type', 'text/html');
+                    fs.createReadStream(filePath).pipe(res);
+                }
+            });
+        } else {
+            res.statusCode = 404;
+            res.setHeader('Content-type', 'text/html');
+            res.end('<html> <body> <h1> error 404:'+ fileURL+ ' not a HTNL file </h1></body></html>');
+        }
+
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-type', 'text/html');
+        res.end('<html> <body> <h1> error 404:'+ fileURL+ ' not supported </h1></body></html>');
+    }
+    
 });
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
+server.listen(port, hostname, () => {
+    console.log(`server running at http://${hostname}:${port}`);
 });
